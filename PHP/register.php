03664b4237f6b5184abd0050d7a421add426d338
Manujaya Vimukthi <mvimukthi2002@gmail.com>
2022-08-08 14:55:38 +0530

    Registerd successfull

--- PHP/register.php
+++ PHP/register.php
@@ -4,12 +4,12 @@
         $password = $_POST['Password'];
 	
 	// Database connection
-	$conn = new mysqli('localhost','root','root','test');
+	$conn = new mysqli('bridalbells.mysql.database.azure.com','Bells@bridalbells','G7@Bridal','bridalbells_db');
 	if($conn->connect_error){
 		echo "$conn->connect_error";
 		die("Connection Failed : ". $conn->connect_error);
 	} else {
-		$stmt = $conn->prepare("insert into register(User_name, Email, Password) values(?, ?, ?)");
+		$stmt = $conn->prepare("insert into customer(User_name, Email, Password) values(?, ?, ?)");
 		$stmt->bind_param("sss", $username, $email, $password);
 		$execval = $stmt->execute();
 		echo $execval;
